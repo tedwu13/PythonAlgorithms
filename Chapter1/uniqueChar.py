57efd655ab0e6ba55ba3724c035104afbf5a30d2
Ted Wu <tedwu2016@u.northwestern.edu>
2015-09-24 00:45:40 -0500

    unique character algorithm in python

---
+++ Chapter1/uniqueChar.py
@@ -0,0 +1,24 @@
+#uniqueChar.py
+
+#Implement an algorithm that would determine if string has all unique characters
+
+def isUniqueChar1(inputString):
+    sortString = ''.join(sorted(inputString))
+    for i in range(0, len(inputString)):
+        if sortString[i] == sortString[i+1]:
+            return False
+        else:
+            return True
+
+def isUniqueChar2(inputString):
+    charList = []
+    for char in inputString:
+        if char in charList:
+            return False
+        else:
+            charList.append(char)
+
+def isUniqueChar3(inputString):
+    if len(inputString) > 256:
+        return False
+    
