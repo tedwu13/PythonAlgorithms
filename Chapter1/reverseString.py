93c476e985715b6c7b9d4071871fe5a3637c1247
Ted Wu <tedwu2016@u.northwestern.edu>
2015-09-27 22:32:15 -0500

    Reverse a string in place

--- Chapter1/reverseString.py
+++ Chapter1/reverseString.py
@@ -1,1 +1,8 @@
 def reverseString(string):
+    output = []
+    for i in range(len(string)-1, -1, -1):
+        output.append(string[i])
+    return ''.join(output)
+        
+print reverseString("hey")
+#expected output should be yeh
