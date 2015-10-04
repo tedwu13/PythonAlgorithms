f3552dccbedeb685e69fc5edaef81619f8f6f6c3
Ted Wu <tedwu2016@u.northwestern.edu>
2015-10-03 20:31:58 -0500

    recursive and iterative solut

--- LeetCode/mergeTwoLinkedLists.py
+++ LeetCode/mergeTwoLinkedLists.py
@@ -1,4 +1,3 @@
-
 def mergeTwoLists(self, l1, l2):
     head = ListNode(0)
     current = ListNode(0)
@@ -16,3 +15,16 @@ def mergeTwoLists(self, l1, l2):
         current = current.next 
     current.next = l1 or l2
     return head.next 
+
+
+def mergeTwoLists(self, l1, l2):
+    if not l1 or not l2:
+        return l1 or l2
+    if l1.val < l2.val:
+        l1.next = self.mergeTwoLists(l1.next, l2)
+        return l1
+    if l2.val < l1.val:
+        l2.next = self.mergeTwoLists(l1, l2.next)
+    return l2
+
+    
