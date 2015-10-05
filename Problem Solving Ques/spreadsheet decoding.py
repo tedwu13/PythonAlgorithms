628693bc575cbed3504772856b646532c24013f8
Ted Wu <tedwu2016@u.northwestern.edu>
2015-10-05 18:27:45 -0500

    encoding spreadsheet

--- Problem Solving Ques/spreadsheet decoding.py
+++ Problem Solving Ques/spreadsheet decoding.py
@@ -7,3 +7,27 @@ def spreadsheet_decoding(str)
         value = value * 26 + c - 'A' + 1
     end
 end
+
+
+def encoding(num)
+  return '' if num == 0
+
+  result = ''
+  while num > 0
+    digit = num % 26
+
+    if digit == 0
+      result = "#{result}Z"
+      num = num / 26 - 1
+    else
+      value = 'A'.ord + digit - 1
+      puts "value: #{value}"
+      result = "#{result}#{value.chr}"
+      num = num / 26
+    end
+
+
+  end
+
+  result.reverse
+end
