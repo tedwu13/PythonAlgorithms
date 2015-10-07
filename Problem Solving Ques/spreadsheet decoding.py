3a679b04ccccfb289bad08cfb1b0a1e676f4c5f0
Ted Wu <tedwu2016@u.northwestern.edu>
2015-10-07 15:24:32 -0500

    strob number

--- Problem Solving Ques/spreadsheet decoding.py
+++ Problem Solving Ques/spreadsheet decoding.py
@@ -6,27 +6,3 @@ def spreadsheet_decoding(str)
         value = value * 26 + char - 'A' + 1
     return value
 
-
-def spreadsheet_encoding(num):
-    
-  return '' if num == 0
-
-  result = ''
-  while num > 0
-    digit = num % 26
-
-    if digit == 0
-      result = "#{result}Z"
-      num = num / 26 - 1
-    else
-      value = 'A'.ord + digit - 1
-      puts "value: #{value}"
-      result = "#{result}#{value.chr}"
-      num = num / 26
-    end
-
-
-  end
-
-  result.reverse
-end
