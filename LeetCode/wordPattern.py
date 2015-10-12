644fdf006713f27075a2431f288b88f242a33d4a
Ted Wu <tedwu2016@u.northwestern.edu>
2015-10-12 15:09:20 -0500

    word pattern

--- LeetCode/wordPattern.py
+++ LeetCode/wordPattern.py
@@ -1,19 +1,5 @@
-def wordPattern(pattern, str):
-    words = str.split()  # Space: O(n)
-    if len(pattern) != len(words):
-        return False
-
-    words, pattern = {}, {}
-    for p, w in zip(pattern, words):
-        print zip(pattern, words)
-        if w not in word and p not in pattern:
-            # Build mapping. Space: O(c)
-            word[w] = p
-            pattern[p] = w 
-        elif w not in w2p or w2p[w] != p:
-            # Contradict mapping.
-            return False
-    return True
+def wordPatter(patter, string):
+    
 
 print wordPattern("abab", "dog cat dog cat")
 print wordPattern("aaaa", "dog dog dog dog")
