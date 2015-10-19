dacdb70c5a31d3c81dfe59175856cea43b283621
Ted Wu <tedwu2016@u.northwestern.edu>
2015-10-19 11:50:23 -0500

    robber problem

---
+++ LeetCode/rob.py
@@ -0,0 +1,16 @@
+public int rob(int[] num) {
+    if(num==null || num.length==0)
+        return 0;
+ 
+    int n = num.length;
+ 
+    int[] dp = new int[n+1];
+    dp[0]=0;
+    dp[1]=num[0];
+ 
+    for (int i=2; i<n+1; i++){
+       dp[i] = Math.max(dp[i-1], dp[i-2]+num[i-1]);
+    }
+ 
+    return dp[n];
+}
