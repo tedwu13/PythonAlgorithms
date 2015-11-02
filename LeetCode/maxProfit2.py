99c696ffb6e4d9ae37e37d3e1d2edb4e1fd84d7f
Ted Wu <tedwu2016@u.northwestern.edu>
2015-11-01 23:15:58 -0600

    max profit 2nd algorithm

---
+++ LeetCode/maxProfit2.py
@@ -0,0 +1,8 @@
+def maxProfit2(array):
+    minimum_price = float("-infinity")
+    maximum_price = float("infinity")
+    for price in prices:
+
+
+#array = [10, 30, 40, 100, 1, 20]
+print maxProfit2([10, 30, 40, 100])
