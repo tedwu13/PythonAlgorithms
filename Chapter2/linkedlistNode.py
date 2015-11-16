f9c584a9d83fe1ffb70cc1ae160274392ef6a620
Ted Wu <tedwu2016@u.northwestern.edu>
2015-11-16 12:36:29 -0600

    get random node

--- Chapter2/linkedlistNode.py
+++ Chapter2/linkedlistNode.py
@@ -1,6 +1,6 @@
 class Node:
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, val):
+        self.val = val
         self.next = None
 
 class LinkedList:
@@ -13,4 +13,24 @@ class LinkedList:
     def removeNode(self, data):
 
     def printLinkedList(self):
-        
+
+
+def printRemovedNode(head, value):
+    if head.val == value:
+        return head.next 
+    while head.next.val != None:
+        if head.next.val == value:
+            temp = head.next
+            head.next = head.next.next
+            return temp
+        else:
+            temp = temp.next
+
+
+def getRandomNode(head):
+
+
+def randomProbability(k):
+    #return true if it is probablity 1/k
+    random = random.randomInt(1,k)
+    if()
