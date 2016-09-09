564f18c316bb958cab51a23b564936f1088b9ca6
Ted Wu <tedwu2016@u.northwestern.edu>
2016-09-08 23:59:47 -0700

    faster sol

--- InterviewCake/max_profit.py
+++ InterviewCake/max_profit.py
@@ -30,4 +30,14 @@ def max_profit(stock_prices_yesterday):
         max_profit = max(max_profit, profit)
     return max_profit
 
-print max_profit([10,7,5,8,11,9])
+# one of the edge case is if the prices keep going down, then we will return max_profit = 0
+def max_profit(stock_prices_yesterday):
+    max_profit = 0
+    min_price = stock_prices_yesterday[0]
+    for current_price in stock_prices_yesterday: 
+        min_price = min(min_price, current_price)
+        profit = current_price - min_price
+        max_profit = max(max_profit, profit)
+    return max_profit
+
+
