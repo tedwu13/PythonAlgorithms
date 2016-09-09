9b788046e5346551ae6fa940fd3e3c00b56ba5c1
Ted Wu <tedwu2016@u.northwestern.edu>
2016-09-08 21:35:56 -0700

    max stock price

---
+++ InterviewCake/max_profit.py
@@ -0,0 +1,33 @@
+
+# O(n^2)
+def max_profit(stock_prices_yesterday):
+    max_profit = 0
+
+    for i in xrange(len(stock_prices_yesterday)):
+        for j in xrange(len(stock_prices_yesterday)):
+            buy_time = min(i, j)
+            sell_time = max(i,j)
+
+            buy_price = stock_prices_yesterday[buy_time]
+            sell_price = stock_prices_yesterday[sell_time]
+
+            potential_profit = sell_price - buy_price
+
+            max_profit = max(potential_profit, max_profit)
+
+    return max_profit
+
+
+
+# keep track of the lowest price
+# check if you can have a bigger profit 
+def max_profit(stock_prices_yesterday):
+    max_profit = 0
+    min_price = stock_prices_yesterday[0]
+    for current_price in stock_prices_yesterday: 
+        min_price = min(min_price, current_price)
+        profit = current_price - min_price
+        max_profit = max(max_profit, profit)
+    return max_profit
+
+print max_profit([10,7,5,8,11,9])
--- LeetCode/wordPattern.py
+++
@@ -1,9 +0,0 @@
-def wordPattern(pattern, string):
-    wordList = string.split()
-    if len(wordList) != len(pattern):
-        return False
-    
-
-print wordPattern("abab", "dog cat dog cat") #True
-print wordPattern("aaaa", "dog dog dog dog") #True
-print wordPattern("abab", "cat cat cat cat") #False
